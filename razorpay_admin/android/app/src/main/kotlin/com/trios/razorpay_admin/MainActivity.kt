78b4fd20bff6a766c5b3192954f76203c31f3edb
Saranraj A <saranraj1820@gmail.com>
2021-12-02 19:07:09 +0530

    init

---
+++ razorpay_admin/android/app/src/main/kotlin/com/trios/razorpay_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.trios.razorpay_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ razorpay_user/android/app/src/main/kotlin/com/trios/razorpay_user/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.trios.razorpay_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
